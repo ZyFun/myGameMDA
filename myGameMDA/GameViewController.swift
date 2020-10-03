79cfeff954b03d9712212d4ae992dd1e726db488
Dmitriy Danilin <danilindn@icloud.com>
2020-10-03 16:04:25 +0300

    конец урока

--- myGameMDA/GameViewController.swift
+++ myGameMDA/GameViewController.swift
@@ -10,19 +10,33 @@ import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    let button = UIButton()
     let label = UILabel()
+    
+    var duration: TimeInterval = 5
+    
+    var score = 0 {
+        didSet {
+            DispatchQueue.main.async {
+                self.label.text = "Score: \(self.score)"
+            }
+        }
+    }
+    
     var ship: SCNNode!
     var scene: SCNScene!
     var scnView: SCNView!
-    
+
+    // MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // create a new scene
         scene = SCNScene(named: "art.scnassets/ship.scn")!
         
-        // Remove scene
+        // remove the ship
         removeShip()
         
         // create and add a camera to the scene
@@ -48,10 +62,14 @@ class GameViewController: UIViewController {
         scene.rootNode.addChildNode(ambientLightNode)
         
         // animate the 3d object
-//        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
+//        ship.runAction(
+//            SCNAction.repeatForever(
+//                SCNAction.rotateBy(x: 0, y: 2 * .pi, z: 0, duration: 1)
+//            )
+//        )
         
         // retrieve the SCNView
-        scnView = self.view as? SCNView
+        scnView = view as? SCNView
         
         // set the scene to the view
         scnView.scene = scene
@@ -69,29 +87,58 @@ class GameViewController: UIViewController {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         scnView.addGestureRecognizer(tapGesture)
         
-        //Add ship to the scene
-        scnView.scene?.rootNode.addChildNode(getShip())
+        // Add ship to the scene
+        ship = getShip()
+        addShip()
         
-        //Add
-        scnView.addSubview(label)
-        label.frame = CGRect(x: 0, y: 0, width: scnView.frame.width, height: 50)
+        // Add label to the scene view
+        label.frame = CGRect(x: 0, y: 0, width: scnView.frame.width, height: 100)
+        label.numberOfLines = 2
         label.textAlignment = .center
         label.font = UIFont.systemFont(ofSize: 30)
+        scnView.addSubview(label)
+        
+        // Add button to the scene view
+        let midX = scnView.frame.midX
+        let midY = scnView.frame.midY
+        let width = CGFloat(200)
+        let height = CGFloat(100)
+        button.addTarget(self,
+                         action: #selector(newGame),
+                         for: .touchUpInside)
+        button.backgroundColor = .red
+        button.layer.cornerRadius = 15
+        button.frame = CGRect(x: midX - width / 2,
+                              y: midY - height / 2,
+                              width: width,
+                              height: height)
+        button.isHidden = true
+        button.setTitle("Restart", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 40)
+        scnView.addSubview(button)
+    }
+    
+    func addShip() {
+        scnView.scene?.rootNode.addChildNode(ship)
     }
     
     func getShip() -> SCNNode {
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
         let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!.clone()
         
-        //Move ship out of view
-        ship.position.z = -105
+        // Move ship out of view
+        let x = Int.random(in: -25...25)
+        let y = Int.random(in: -25...25)
+        let z = -105
+        ship.position = SCNVector3(x, y, z)
+        ship.look(at: SCNVector3(2 * x, 2 * y, 2 * z))
         
-        //Add ship animation
-        ship.runAction(.move(to: SCNVector3(), duration: 5)) {
+        // Add ship animation
+        ship.runAction(.move(to: SCNVector3(), duration: duration)) {
             ship.removeFromParentNode()
-            print(#line, #function, "Animation ended")
             DispatchQueue.main.async {
-                self.label.text = "Game Over"
+                self.button.isHidden = false
+                self.label.text = "Game Over\nScore: \(self.score)"
             }
         }
         
@@ -100,6 +147,15 @@ class GameViewController: UIViewController {
     
     func removeShip() {
         scene?.rootNode.childNode(withName: "ship", recursively: true)?.removeFromParentNode()
+    }
+    
+    // MARK: - Actions
+    @objc func newGame() {
+        duration = 5
+        score = 0
+        button.isHidden = true
+        ship = getShip()
+        addShip()
     }
     
     @objc
@@ -120,16 +176,16 @@ class GameViewController: UIViewController {
             
             // highlight it
             SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
+            SCNTransaction.animationDuration = 0.2
             
             // on completion - unhighlight
             SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
+                self.ship.removeAllActions()
+                self.ship.removeFromParentNode()
+                self.duration *= 0.95
+                self.ship = self.getShip()
+                self.addShip()
+                self.score += 1
             }
             
             material.emission.contents = UIColor.red
@@ -138,6 +194,7 @@ class GameViewController: UIViewController {
         }
     }
     
+    // MARK: - Computed Properties
     override var shouldAutorotate: Bool {
         return true
     }
